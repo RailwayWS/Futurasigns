dbc572fbb2bd79edf16d4804713c35eeabcf23e6
Ruben Smith <27295451@sun.ac.za>
2025-09-09 12:37:31 +0200

    Added contact us page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import ad from './assets/ad.png'
 import create from './assets/creativity.png'
 import relocate from './assets/relocation.png'
 import trust from './assets/trust.png'
+import location_pin from './assets/location_pin.png'
+import email_logo from './assets/email.png'
+import phone_logo from './assets/phone.png'
 
 export default function App() {
   return (
@@ -13,6 +16,7 @@ export default function App() {
       <Navbar />
       <Home />
       <About />
+        <Contact />
     </PrimeReactProvider>
   );
 }
@@ -91,4 +95,60 @@ function About(){
                                   In fact, let's build this country!</div>
     </Element>
   );
+}
+
+function Contact() {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+    }
+
+    return (
+        <Element name='contact' className='contact-container'>
+            <div className='contact-info'>
+                <div className='contact-card'>
+                    <img src={location_pin} alt="pin"/>
+                    <h3> Address</h3>
+                    <p>9 Printers Way, Marconi Beam, Cape Town, 7447</p>
+                </div>
+                <div className='contact-card'>
+                    <img src={email_logo} alt="email logo"/>
+                    <h3> Email </h3>
+                    <p>futurasales@mweb.co.za</p>
+                </div>
+                <div className='contact-card'>
+                    <img src={phone_logo} alt="phone logo"/>
+                    <h3> Call Us </h3>
+                    <p>021 551 1814</p>
+                </div>
+            </div>
+
+            <form className='contact-form' action="https://api.web3forms.com/submit" method="POST">
+
+                <input type="hidden" name="access_key" value="31154c19-76f9-4b6b-a4ba-8aa9f8ac3a6c"/>
+
+                <h2> Contact Us</h2>
+                <input
+                    type='text'
+                    name="full-name"
+                    placeholder="Name and Surname"
+                />
+
+                <input
+                    type='email'
+                    name="email"
+                    placeholder="Your Email Address"
+                />
+
+                <textarea
+                    type="text"
+                    name="message"
+                    placeholder="Message..."
+                />
+
+                <button> Send</button>
+            </form>
+        </Element>
+    )
 }
