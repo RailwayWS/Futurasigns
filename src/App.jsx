b7857f44b95c153153a179f18e7ab5cf80a1f6ac
Jacob Johannes Mostert <27038122@sun.ac.za>
2025-09-09 11:18:59 +0200

    added about section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import { PrimeReactProvider } from 'primereact/api';
 import { Link, Element } from "react-scroll";
 import { Button } from 'primereact/button';
 import placeholder1 from './assets/Placeholders/ph1.jpeg'
+import ad from './assets/ad.png'
+import create from './assets/creativity.png'
+import relocate from './assets/relocation.png'
+import trust from './assets/trust.png'
 
 export default function App() {
   return (
     <PrimeReactProvider>
       <Navbar />
       <Home />
+      <About />
     </PrimeReactProvider>
   );
 }
@@ -18,7 +23,6 @@ function Navbar() {
       <div className='nav-container'>
         <Link to="home" smooth={true} duration={500} className="link-item">Home</Link>
         <Link to="about" smooth={true} duration={500} className="link-item">About</Link>
-        <Link to="services" smooth={true} duration={500} className="link-item">Services</Link>
         <Link to="portfolio" smooth={true} duration={500} className="link-item">Portfolio</Link>
         <Link to="contact" smooth={true} duration={500} className="link-item"> <Button label="Contact Us" rounded /> </Link>
       </div>
@@ -28,16 +32,63 @@ function Navbar() {
 
 function Home(){
   return (
-    <div className='hero-container'>
+    <Element name='home' className='hero-container'>
       <div className='hero-info'>
-        <h1>We listen & deliver custom solutions to your business.</h1>
-        <p>Established in 1986, Futura Signs focusses on supplying and installing large billboards and signboards for property developments. Over the years we have manufactured and installed billboards for many property developments, we have also handled several billboard campaigns for signature golf estates and other major property ventures.</p>
+        <h1>Your Trusted Signage Partner in Cape Town</h1>
+        <p>Established in 1986, Futura Signs is Cape Town’s trusted partner for large-format billboards and custom signboards.  We listen first, then deliver tailored solutions that help your brand stand out. Our in-house team designs, manufactures and installs everything from high-impact roadside billboards to professional on-site development boards, indoor branding, vehicle graphics and banners. Over the years we’ve produced and installed billboards and full campaigns for major property developments, golf estates, wine farms, retail centres, corporate brands and other leading ventures across the Western Cape. From concept, engineering and council approvals to fabrication, project management and safe on-site installation, we handle the entire process end-to-end—on time and to spec. Whether you need a single billboard or a multi-site rollout, expect quality materials, precise workmanship and a reliable finish that thrives in Cape Town’s conditions.</p>
         <div className='hero-buttons'>
-          <button> Schedule a call</button>
-          <button className='button-invert'> Download Pricing</button>
+          <button> See our work</button>
+          <button className='button-invert'> Find us</button>
         </div>
       </div>
       <img src={placeholder1} alt="hero image" />
-    </div>
+    </Element>
+  );
+}
+
+function About(){
+  return (
+    <Element name='about' className='about-container'>
+      <div className='about-heading'>
+        <h2> About us</h2>
+        <div className='about-text'>Futura Signs has been turning heads since 1986</div>
+      </div>
+      <div className='about-info'>
+        <div className='about-card'>
+          <img src={ad} alt="ad" />
+          <div className='card-heading'>Buildboards Built to Last</div>
+          <div className='card-text'>
+            We are specialists in billboards — it’s our passion and our craft. From design to fabrication to installation, we build billboards that are made to last. Over the decades, we've become a trusted name in outdoor advertising, known for structures that stand the test of time.
+          </div>
+        </div>
+
+         <div className='about-card'>
+          <img src={relocate} alt="ad" />
+          <div className='card-heading'>Relocation & <br/>Refurbishment Services</div>
+          <div className='card-text'>
+            For our long-term clients, we offer a unique service: billboard relocation and refurbishment. We'll carefully dismantle your billboard, store it securely, and give it a new lease on life at a fresh location — a rare offering in our industry.
+          </div>
+        </div>
+
+         <div className='about-card'>
+          <img src={create} alt="ad" />
+          <div className='card-heading'>Custom Signage Solutions</div>
+          <div className='card-text'>
+            While billboards are our bread and butter, we also create a wide range of custom signage. If you need it, just ask — chances are, we can make it.
+          </div>
+        </div>
+
+         <div className='about-card'>
+          <img src={trust} alt="ad" />
+          <div className='card-heading'>Your Trusted Signage Partner</div>
+          <div className='card-text'>
+            We’re a vibrant, experienced team that believes in doing great work and building lasting relationships. With a reputation for quality craftsmanship, excellent client service, and competitive pricing, Futura Signs is your reliable signage partner in Cape Town and beyond.
+          </div>
+        </div>
+        
+      </div>
+      <div className='about-text'>Let’s build something bold together. <br/>
+                                  In fact, let's build this country!</div>
+    </Element>
   );
 }
